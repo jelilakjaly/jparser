c2c8c4be7dd1b5a3ac0c4250eff40615195ea2e1
Jelil Akjaly <jelil.akjaly@gmail.com>
2022-06-12 14:29:32 +0430

    Created Parser newtype and implemented Functor, Applicative, Monad, Alternative instances for it

--- src/JParse.hs
+++ src/JParse.hs
@@ -1,4 +1,29 @@
-module JParse (someFunc) where
+module JParse where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import           Control.Applicative
+
+newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+
+instance Functor Parser where
+    fmap f (Parser x) = Parser $ \s -> do
+        (a, s') <- x s
+        return (f a, s')
+
+instance Applicative Parser where
+    pure x = Parser $ \s -> Just (x, s)
+    Parser sfs <*> Parser sas = Parser $ \s -> do
+        (f, s') <- sfs s 
+        (a, s'') <- sas s' 
+        return (f a, s'')
+
+instance Monad Parser where
+    Parser sas >>= f = Parser $ \s -> do
+        (a', s') <- sas s
+        runParser (f a') s'
+
+instance Alternative Parser where
+    empty = Parser $ const Nothing
+    Parser x <|> Parser y = Parser $ \s -> case x s of
+        Nothing -> y s
+        Just v  -> Just v
+
