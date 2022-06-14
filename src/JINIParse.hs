f74fba827593afae4b635a8dfa3c24d21daccfd1
Jelil Akjaly <jelil.akjaly@gmail.com>
2022-06-14 22:04:19 +0430

    updated

--- src/JINIParse.hs
+++ src/JINIParse.hs
@@ -1,6 +1,7 @@
 module JINIParse where
 
 import Control.Applicative
+import Data.Map hiding (empty)
 
 import JParse
 
@@ -12,6 +13,10 @@ import JParse
 -- [ section2]
 -- name = value
 -- name2 = "quoted value 2"
+
+type Variables = Map String String
+type Sections = Map String Variables
+newtype INI = INI Sections deriving Show
 
 bracketed :: Parser a -> Parser b -> Parser c -> Parser b
 bracketed pa pb pc = do 
@@ -36,3 +41,61 @@ sectionHeader = bracketed bracketOpen sectionName bracketClose
 name :: Parser String 
 name = some alphanum
 
+quotedValue :: Parser String 
+quotedValue = bracketed quote (many alphaNumSpace) quote
+
+value :: Parser String 
+value = name <|> quotedValue
+
+
+-- an assignment is a name value pair separated by '=' character
+-- we ignore the whitespace around these
+assignment :: Parser (String, String)
+assignment = do 
+    spaces
+    name <- name 
+    spaces 
+    char '='
+    spaces 
+    value <- value 
+    return (name, value)
+
+
+-- A section has a section header and name-value pairs 
+-- separated by '=' character
+newline :: Parser Char 
+newline = char '\n' <|> char '\r'
+
+newlines :: Parser ()
+newlines = many newline >> return ()
+
+blank :: Parser ()
+blank = spaces >> newline >> return ()
+
+blanks :: Parser ()
+blanks = many blank >> return ()
+
+assignments :: Parser Variables
+assignments = fromList <$> many (blanks >> assignment)
+
+section :: Parser (String, Variables)
+section = do 
+    blanks 
+    spaces 
+    name <- sectionHeader
+    blanks 
+    variables <- assignments 
+    return (name, variables)
+
+
+ini :: Parser INI 
+ini = INI . fromList <$> many section
+
+
+sampleText = 
+    "[section]" ++ "\n" ++
+    "name = value" ++ "\n" ++
+    "name2 = \"quoted value\"" ++ "\n" ++
+    "[section2]" ++ "\n" ++
+    "name = value" ++ "\n" ++
+    "name2 = \"quoted value 2\""
